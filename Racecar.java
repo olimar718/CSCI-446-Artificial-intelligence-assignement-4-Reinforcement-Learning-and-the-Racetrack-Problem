4af6890f2ef2e09c0466ed6e0658cd4f3210c2d3
unknown <Cathelineau Benjamin@DESKTOP-Benjamin-Windows.lan>
2020-04-21 17:16:08 +0200

    progress
    Co-authored-by: Braypolk <braypolk@gmail.com>

--- Racecar.java
+++ Racecar.java
@@ -77,10 +77,11 @@ public class Racecar {
         this.state.xPosition += this.state.xSpeed;
         this.state.yPosition += this.state.ySpeed;
 
-        // check track boundaries
+        // check track boundaries and if it 
         int reward;
         if (this.state.xPosition >= course.length || this.state.yPosition >= course[0].length
-                || this.state.xPosition < 0 || this.state.yPosition < 0) {
+                || this.state.xPosition < 0 || this.state.yPosition < 0
+                || bresenham(this.state.xPosition, this.state.yPosition, previousXPostion, previousYPosition, course)) {
 
             this.state.xSpeed = 0;
             this.state.ySpeed = 0;
@@ -99,5 +100,53 @@ public class Racecar {
         }
 
         return reward;
+    }
+
+    boolean bresenham(int x1, int y1, int x2, int y2, char[][] course) {
+        int m_new = 2 * (y2 - y1);
+        int slope_error_new = m_new - (x2 - x1);
+        Boolean crossedWall = Boolean.FALSE;
+        for (int x = x1, y = y1; x <= x2; x++) {
+            // check for bound
+            Boolean wasOutOfBound = Boolean.FALSE;
+            int newx = x;
+            int newy = y;
+            if (x >= course.length) {
+                wasOutOfBound = Boolean.TRUE;
+                newx = course.length - 1;
+            }
+            if (y >= course[0].length) {
+                wasOutOfBound = Boolean.TRUE;
+                newy = course[0].length - 1;
+            }
+            if (x < 0) {
+                wasOutOfBound = Boolean.TRUE;
+                newx = 0;
+            }
+            if (y < 0) {
+                wasOutOfBound = Boolean.TRUE;
+                newy = 0;
+            }
+
+            // check if we cross a wall
+            if (wasOutOfBound) {
+                if (course[newx][newy] == '#') {
+                    crossedWall = Boolean.TRUE;
+                }
+
+            } else {
+                if (course[x][y] == '#') {
+                    crossedWall = Boolean.TRUE;
+                }
+            }
+
+            slope_error_new += m_new;
+
+            if (slope_error_new >= 0) {
+                y++;
+                slope_error_new -= 2 * (x2 - x1);
+            }
+        }
+        return crossedWall;
     }
 }
