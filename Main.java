11c7ef0739333a4ab052c9ab023b71330c4cbfee
unknown <Cathelineau Benjamin@DESKTOP-Benjamin-Windows.lan>
2020-05-04 15:03:04 +0200

    script edit

--- Main.java
+++ Main.java
@@ -4,11 +4,11 @@
 
 public class Main {
     public static void main(String[] args) {
-        String filename = "O-track.txt";
+        String filename = "L-track.txt";
         ReadFile read = new ReadFile(filename);
         char[][] course = read.getCourse();
         Racecar racecar = new Racecar(course);
-        //new ValueIteration(course, 10, .99, Boolean.TRUE);
-        new Qlearning(racecar, course,0.8,0.3,Boolean.FALSE);
+        new ValueIteration(course, 10, .99, Boolean.FALSE);
+        //new Qlearning(racecar, course,0.8,0.3,Boolean.FALSE);
     }
 }
